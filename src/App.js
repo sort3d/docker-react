e859c505ea71bb696fcbb56bf2e9907771c2b1e7
David Romero <david.romero@gmail.com>
2019-05-25 10:58:51 -0400

    Update welcome message again.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload, big guy!
+          Edit <code>src/App.js</code> and save to reload, big guy!  
+          And don't forget to like and subscribe.
         </p>
 
         <a
